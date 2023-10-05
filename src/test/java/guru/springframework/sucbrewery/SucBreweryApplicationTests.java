fdf1d42dc945dfd605b627aab2ac99514e490d6d
João Machado <jtomaspm@ua.pt>
2023-10-05 19:00:12 +0100

    BATMAN

---
+++ src/test/java/guru/springframework/sucbrewery/SucBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sucbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SucBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
